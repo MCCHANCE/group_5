0527307b978d798d99e7f71b66a9483d3d903a43
BRIGHT (Bwave) <ivquestservices@gmail.com>
2023-03-31 21:34:03 +0100

    Update code.sh

--- shell/var/practise/questions/code.sh
+++ shell/var/practise/questions/code.sh
@@ -2,4 +2,23 @@
 
 #Turn the thought process in the file called thought_process to a shell script code.
 
+# Hello World Program in Bash Shell
+
+read -p "What is your name: " name
+
+read -p "What is your cohort: " cohort
+
+if [ $cohort -gt 14 ]
+then
+echo "Error, You cannot be in cohort $cohort because we dont have that cohort yet"
+else
+read -p "What is your score: " score
+
+if [ $score -gt 170 ]
+then
+echo "Error, Your score can not be up to $score."
+else 
+echo "Hello, your name is $name, your cohort is $cohort, and your score is $score"
+fi
+fi
 
