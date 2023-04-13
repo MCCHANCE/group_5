8fde9ac974cdada300ea4fd80d8767add47376da
brightdaniel <ivquestservices@gmail.com>
2023-04-13 20:55:37 +0100

    operators

---
+++ c-programming/operators.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+int main()
+{
+	/*  OPREATORS IN C. */
+
+	/* LOGICAL OPERATORS 
+	 * >    = greater than  a > b
+	 * <    = lesser than b
+	 * =    = gets the value of b
+	 * ==   = equal to b 
+	 * */
+
+	int a = 10;
+
+	int b = 5;
+
+	int c =  a == b;
+
+	printf("%d", c);
+
+
+
+	/* ARITHEMETIC OPERATORS
+	 *
+	 * +  add
+	 * -  sub
+	 *     * mul
+	 *      / divide
+	 *       ++ icrease by 1
+	 *       -- decrease by 1
+	 *        % mod - finds the remainder of something.
+	 *
+	 *          */
+	  
+	int a = 2594;
+
+	int b = 4;
+
+	int c = a % 10;
+	  
+	printf("%d", c);
+
+
+
+
+}
