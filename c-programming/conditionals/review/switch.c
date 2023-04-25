66bb6c5454ef311b3a195ca67f53fb45fc8f0667
brightdaniel <ivquestservices@gmail.com>
2023-04-25 21:13:20 +0100

    review

---
+++ c-programming/conditionals/review/switch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	 //TODO
+	 // using switch statements, depending on the number of the day, print out the day of the week, and tell us if it is a weekday, or weekend.
+	
+
+
+}
---
+++ c-programming/conditionals/review/upper_check.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+	char input;
+
+	//TODO
+	//complete this part to make our code functional.
+
+
+	//TODO
+	//redo this part using ascil values.
+	if (input >= 'a' & input <= 'z')
+	{
+		printf("%c is a lowercase alphabelt \n");
+	}
+	else if (input >= 'A' & input <= 'Z')
+	{
+		printf("%c is a uppercase alphabelt \n");
+	}
+	else
+		//TODO
+		//complete this part to print out an error if input is not char.
+
+}
