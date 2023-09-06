2b48bfde0179520e936dc86cdbcc39d787e58f44
MCCHANCE <unwananyong2021@gmail.com>
2023-09-05 18:29:07 -0700

    updated

---
+++ c-programming/arrays.c/scores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	//enter scores
+	int score1 = 72;
+	int score2 = 73;
+	int score3 = 88;
+
+	    // Print average
+            printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+}
